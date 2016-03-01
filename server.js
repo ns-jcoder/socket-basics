df389b89304c6e51d13c6880feb8e17adab5954f
Naeem Shaikh <nayem.ns@gmail.com>
2016-03-01 19:29:49 +0530

    Add support for currnetUsers command

--- server.js
+++ server.js
@@ -15,13 +15,35 @@ var clientInfo = {
 	// }
 };
 
+//Sends current users to provided socket
+
+function sendCurrentUsers (socket){
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if(typeof info === 'undefined'){
+		return ;
+	}
+	Object.keys(clientInfo).forEach(function (socketId){
+		var userInfo = clientInfo[socketId];
+		if(info.room === userInfo.room){
+			users.push(userInfo.name);
+		}
+	});
+	socket.emit('message',{
+		 name: 'System',
+		 text: 'Current users '+ users.join(', '),
+		 timestamp: moment.valueOf()
+	})
+}
+
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
 
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function() {
 		var userData = clientInfo[socket.id];
-		if(typeof userData !== 'undefined'){
+		if (typeof userData !== 'undefined') {
 			socket.leave(userData.room);
 			io.to(userData.room).emit('message', {
 				name: 'System',
@@ -32,8 +54,8 @@ io.on('connection', function(socket) {
 		}
 	});
 
-	socket.on('joinRoom', function (req){
-		clientInfo[socket.id] = req; 
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
 		socket.join(req.room);
 		socket.broadcast.to(req.room).emit('message', {
 			name: 'System',
@@ -41,12 +63,18 @@ io.on('connection', function(socket) {
 			timestamp: moment.valueOf()
 		});
 	});
-	
 
-	socket.on('message',function (message){
-		message.timestamp = moment().valueOf();
-		console.log('message recived:'+ message.text)
-		io.to(clientInfo[socket.id].room).emit('message',message);
+
+	socket.on('message', function(message) {
+		console.log('message recived:' + message.text)
+
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+
+		}
 	})
 
 	socket.emit('message', {
