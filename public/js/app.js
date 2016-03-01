2bfa86811ee0bd246c84ddacf0882fb581438fa7
Naeem Shaikh <nayem.ns@gmail.com>
2016-03-01 12:08:39 +0530

    Caht room

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,43 @@
 //app.js
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
 var socket = io();
 
-socket.on('connect',function () {
+console.log(name + 'Wants to join ' + room);
+
+// Update h1 tag
+jQuery('.room-title').text(room);
+
+socket.on('connect', function() {
 	console.log('Connected to socket.io server')
 
 });
 
-socket.on('message',function (message){
+socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages')
 
 	console.log('New message: ')
 	console.log(message.text)
 
-	jQuery('.messages').append('<p> <strong> '+ momentTimestamp.local().format('h:mm a') +': </strong> ' +message.text + '</p>');
+	$message.append('<p> <strong>' + message.name  + ' ' + momentTimestamp.local().format('h:mm a') + ': </strong> ' + message.text + '</p>')
+	$message.append('<p> ' + message.text + '</p>');
+	//jQuery('.messages').append('<p> <strong> ' + momentTimestamp.local().format('h:mm a') + ': </strong> ' + message.text + '</p>');
 });
 
 //Handles submitting of new message
 
 var $form = jQuery('#message-form');
 console.log($form);
-$form.on('submit', function (event){
+$form.on('submit', function(event) {
 	event.preventDefault();
 
 	var $message = $form.find('input[name=message]');
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	})
-$message.val('');
+	$message.val('');
 
 });
