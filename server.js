c46b76a0849c0068f6df9a331c0561aa8379ac69
Naeem Shaikh <nayem.ns@gmail.com>
2016-03-01 18:16:54 +0530

    Add support for chat rooms and dlete user

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ var clientInfo = {
 io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
 
+
+	socket.on('disconnect', function(){
+		var userData = clientInfo[socket.id];
+		if(typeof userData !== 'undefined'){
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function (req){
 		clientInfo[socket.id] = req; 
 		socket.join(req.room);
@@ -32,7 +46,7 @@ io.on('connection', function(socket) {
 	socket.on('message',function (message){
 		message.timestamp = moment().valueOf();
 		console.log('message recived:'+ message.text)
-		io.to(clientInfo[socket.id.room]).emit('message',message);
+		io.to(clientInfo[socket.id].room).emit('message',message);
 	})
 
 	socket.emit('message', {
